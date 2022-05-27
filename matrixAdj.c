154219c659e62fa492a144dc63272574b563e0d4
PatrickSilvaMenezes <patricksilvamenezes@gmail.com>
2022-05-27 10:15:00 -0300

    subindo algoritmos de busca BFS e DFS da materia de AEDS2

---
+++ matrixAdj.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct Graph *Graph;
+
+struct Graph{
+    int **matAdj;
+    int u; //numero de vertices
+    int v; //numero de arestas
+};
+
+Graph GraphInitialize(int u)
+{
+
+}
+
+void GraphInsertEdge()
+{
+
+}
+
+void ImprimeGraph(Graph G)
+{
+
+}
+
+void BFS()
+
+
+int main()
+{
+
+
+    return 0;
+}
